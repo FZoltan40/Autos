bcd4ccd44b3ed4a66130a3ce292613e59b1c070c
Farkas <farasz@kkszki.hu>
2024-09-24 12:47:41 +0200

    Connect class is created.

---
+++ Connect.cs
@@ -0,0 +1,21 @@
+﻿namespace Auto
+{
+    public class Connect
+    {
+        private string Host;
+        private string Database;
+        private string Username;
+        private string Password;
+        private string ConnectionString;
+
+        public Connect()
+        {
+            Host = "localhost";
+            Database = "auto";
+            Username = "root";
+            Password = "";
+
+            ConnectionString = "SERVER=" + Host + ";DATABASE=" + Database + ";UID=" + Username + ";PASSWORD=" + Password + ";SslMode=None";
+        }
+    }
+}
